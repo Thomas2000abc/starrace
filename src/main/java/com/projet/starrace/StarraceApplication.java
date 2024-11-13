673eb47880c3886f5e4a57845d24c47f10fa5349
nostenorge <nosten.official@gmail.com>
2024-11-13 14:14:55 +0100

    test

--- src/main/java/com/projet/starrace/StarraceApplication.java
+++ src/main/java/com/projet/starrace/StarraceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarraceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StarraceApplication.class, args);
+
 	}
 
 }
