26dacb596b5c9362589506ed359d7e718abdb946
nostenorge <nosten.official@gmail.com>
2024-11-13 14:16:05 +0100

    test2

--- src/main/java/com/projet/starrace/StarraceApplication.java
+++ src/main/java/com/projet/starrace/StarraceApplication.java
@@ -9,6 +9,7 @@ public class StarraceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(StarraceApplication.class, args);
+		System.out.println("coucou c'est moi");
 
 	}
 
