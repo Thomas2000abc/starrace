21001e8dcea286b7bce651a0c66bcb29e33a0aa2
Steven <steven.mahouasse1999@gmail.com>
2024-11-13 14:53:21 +0100

    Création d'account et match entity

--- src/main/java/com/projet/starrace/entity/Match.java
+++ src/main/java/com/projet/starrace/entity/Match.java
@@ -1,4 +1,22 @@
 package com.projet.starrace.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "match")
 public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_match")
+    private int idMatch;
+    @Column(name = "home_team_id")
+    private int homeTeamId;
+    @Column(name = "away_team_id")
+    private int awayTeamId;
+    @Column(name = "tournament_id")
+    private int tournamentId;
+    @Column(name = "home_goals")
+    private int homeGoals;
+    @Column(name = "away_goals")
+    private int awayGoals;
 }
