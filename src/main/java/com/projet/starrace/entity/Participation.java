acb10c8d88444eaebbe45122ff6dadcb9c771c6e
Thomas Reuter <thomas2000abc@gmail.com>
2024-11-13 15:47:44 +0100

    test

--- src/main/java/com/projet/starrace/entity/Participation.java
+++ src/main/java/com/projet/starrace/entity/Participation.java
@@ -1,4 +1,54 @@
 package com.projet.starrace.entity;
 
+import com.projet.starrace.enumeration.Position;
+import com.projet.starrace.enumeration.Rarity;
+import jakarta.persistence.*;
+import org.hibernate.type.descriptor.jdbc.TinyIntJdbcType;
+
 public class Participation {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "validity")
+    private int validity;
+
+
+    @ManyToOne
+    @JoinColumn(name = "tournament_id")
+    @Column(name = "tournament_id")
+    private Tournaments tournament;
+
+    @ManyToOne
+    @JoinColumn(name = "teams_id")
+    @Column(name = "teams_id")
+    private Teams team;
+
+    public Participation(int validity) {
+        this.validity = validity;
+    }
+
+    public int getValidity() {
+        return validity;
+    }
+
+    public void setValidity(int validity) {
+        this.validity = validity;
+    }
+
+    public Tournaments getTournament() {
+        return tournament;
+    }
+
+    public void setTournament(Tournaments tournament) {
+        this.tournament = tournament;
+    }
+
+    public Teams getTeam() {
+        return team;
+    }
+
+    public void setTeam(Teams team) {
+        this.team = team;
+    }
 }
---
+++ src/main/java/com/projet/starrace/enumeration/Position.java
@@ -0,0 +1,13 @@
+package com.projet.starrace.enumeration;
+
+public enum Position {
+    GK,
+    DC,
+    DG,
+    DD,
+    MC,
+    MOC,
+    BU,
+    AG,
+    AD,
+}
